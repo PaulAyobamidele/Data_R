ef52d8a349f160444500d3be858439e2143565c3
Ayobamidele <paulikeystreble@gmail.com>
2023-12-06 11:26:06 +0100

    Iris dataset added

---
+++ Iris/IrisOne.r
@@ -0,0 +1,49 @@
+# Load the dataset 'your_dataset' and explore its structure
+
+# 1.1 Describe the basic statistics of the numerical variables in the dataset
+
+# 1.2 Create a boxplot or histogram for a numerical variable to visualize its distribution
+
+# 1.3 Calculate the correlation matrix for the numerical variables
+
+# 1.4 Visualize the relationship between two numerical variables using a scatter plot
+
+# 1.5 Perform a simple linear regression to explore the association between two variables
+# Load the dataset 'your_dataset' and explore its structure
+
+# 2.1 Perform a t-test or ANOVA to compare the means of a numerical variable across different groups
+
+# 2.2 Visualize the distribution of a numerical variable for different categories using boxplots
+
+# 2.3 Calculate the confidence interval for the mean of a numerical variable
+
+# 2.4 Conduct a chi-square test to explore the relationship between two categorical variables
+# Load the time series dataset 'your_time_series_dataset' and explore its structure
+
+# 3.1 Plot the time series of the data to observe trends and patterns
+
+# 3.2 Decompose the time series into trend, seasonal, and residual components
+
+# 3.3 Conduct a statistical test for stationarity (e.g., ADF test)
+
+# 3.4 Create a lag plot to explore autocorrelation in the time series
+# Load a classification dataset 'your_classification_dataset' and explore its structure
+
+# 4.1 Split the dataset into training and testing sets
+
+# 4.2 Train a classification model (e.g., decision tree or logistic regression)
+
+# 4.3 Evaluate the model on the test set using accuracy and confusion matrix
+
+# 4.4 Tune hyperparameters or try different algorithms to improve model performance
+# Load a dataset 'your_visualization_dataset' for data visualization
+
+# 5.1 Create a scatter plot to visualize the relationship between two numerical variables
+
+# 5.2 Generate a bar chart or pie chart to show the distribution of a categorical variable
+
+# 5.3 Create a heatmap to visualize the correlation matrix of numerical variables
+
+# 5.4 Design a customized plot (e.g., faceted plot or multi-line plot) to convey specific insights
+
+# 5.5 Utilize ggplot2 or another visualization library for advanced data representation
--- Titanic/titanic_one.r
+++ Titanic/titanic_one.r
@@ -17,3 +17,17 @@ chisq.test(contigency_table)
 survived_data <- factor(titanic_train$Survived, levels = c(0, 1), labels = c("Not Survived", "Survived"))
 ggplot(titanic_train) +
     geom_bar(aes(Pclass, fill = survived_data), position = "dodge")
+
+
+
+
+# PERCENTILE
+
+
+?quakes
+
+
+sumarry <- quantile((quakes$mag[quakes$depth < 400]), prob = c(0, 0.25, 0.5, 0.75, 1))
+sumarry
+
+summary(quakes$mag[quakes$depth < 400])
